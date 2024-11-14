c0dd37a28f6d99464e1b4114aa36adebd4c28d4f
Rifatul Islam <rifatulcse@gmail.com>
2024-11-14 17:11:25 +0600

    feat: Chart of accounts page added

---
+++ app/(app)/settings/chart-of-accounts/page.tsx
@@ -0,0 +1,12 @@
+import ChartOfAccounts from '@/components/settings/chartofaccounts'
+import React from 'react'
+
+const ChartOfAccountsPage = () => {
+    return (
+        <div>
+            <ChartOfAccounts />
+        </div>
+    )
+}
+
+export default ChartOfAccountsPage
