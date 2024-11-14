876fe286107816c248eb0a42c9f5233423f5add5
Akil Tajwar <akiltajwar@gmail.com>
2024-11-14 16:24:54 +0600

    feat: users-list

---
+++ app/(app)/settings/users-list/page.tsx
@@ -0,0 +1,12 @@
+import UsersList from '@/components/users-list/users-list'
+import React from 'react'
+
+const UsersListPage = () => {
+    return (
+        <div>
+            <UsersList />
+        </div>
+    )
+}
+
+export default UsersListPage
