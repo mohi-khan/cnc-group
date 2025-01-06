a87390c6697493da4ca9faf102c5e8d06c02357a
Rifatul Islam <rifatulcse@gmail.com>
2025-01-06 16:07:29 +0600

    fix: chart of accounts acitve or deactive button and feat: trial balance headding section and table section get create api and show data in table

--- api/trial-balance-api.ts
+++ api/trial-balance-api.ts
@@ -1,9 +1,9 @@
+import { TrialBalanceData } from '@/components/bank/trial-balance/trial-balance-table'
 import { fetchApi } from '@/utils/http'
-import { BankAccountDateRange } from '@/utils/type'
 
 export async function getTrialBalance() {
-  return fetchApi<BankAccountDateRange[]>({
-    url: `http://localhost:4000/api/ledgerreport/trialBalance?fromdate=2024-01-01&enddate=2024-12-31&companyid=93`, //static API
+  return fetchApi<TrialBalanceData[]>({
+    url: 'api/ledgerreport/trialBalance?fromdate=2024-01-01&enddate=2024-12-31&companyid=93', //static API
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
