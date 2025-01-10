063ecf1eade7bff8400efc50293c815d64518f0c
Akil Tajwar <akiltajwar@gmail.com>
2025-01-11 01:21:56 +0600

    feat: general ledger done

---
+++ app/(app)/general-ledger/page.tsx
@@ -0,0 +1,12 @@
+import GeneralLedger from '@/components/general-ledger/general-ledger';
+import React from 'react';
+
+const GeneralLedgerPage = () => {
+    return (
+        <div>
+            <GeneralLedger />
+        </div>
+    );
+};
+
+export default GeneralLedgerPage;
