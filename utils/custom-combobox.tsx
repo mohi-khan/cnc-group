415e9c545a0c2aec0e9459a7f8ed5efefedf25c5
rifatulislamr <rifatulislamr>
2025-02-03 02:18:59 +0600

    refactor:custom combobox solve some issues

--- utils/custom-combobox.tsx
+++ utils/custom-combobox.tsx
@@ -38,7 +38,7 @@ export function CustomCombobox<T extends ComboboxItem>({
       <div className="relative">
         <div className="relative w-full cursor-default overflow-hidden border rounded-lg bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
           <Combobox.Input
-            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-3 text-gray-900 focus:ring-0"
             displayValue={(item: T | null) => item?.name || ''}
             onChange={(event) => setQuery(event.target.value)}
             placeholder={placeholder}
@@ -57,7 +57,8 @@ export function CustomCombobox<T extends ComboboxItem>({
           leaveTo="opacity-0"
           afterLeave={() => setQuery('')}
         >
-          <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+         
+         <Combobox.Options className="absolute z-20 mt-1 h-24  overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-black/5 focus:outline-none">
             {filteredItems.length === 0 && query !== '' ? (
               <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
                 Nothing found.
@@ -67,7 +68,7 @@ export function CustomCombobox<T extends ComboboxItem>({
                 <Combobox.Option
                   key={item.id}
                   className={({ active }) =>
-                    `relative cursor-default select-none py-2 pl-10 pr-4 mx-2 rounded-md ${
+                    `relative cursor-default select-none py-1 px-10  mx-2 rounded-md ${
                       active ? 'bg-slate-200 text-black' : 'text-gray-900'
                     }`
                   }
@@ -77,7 +78,7 @@ export function CustomCombobox<T extends ComboboxItem>({
                     <div className="flex items-center">
                       {selected ? (
                         <span
-                          className={` inset-y-0 left-0 flex items-center pl-3 ${
+                          className={` inset-y-0 left-0 flex items-center  ${
                             active ? 'text-teal-600' : 'text-teal-600'
                           }`}
                         >
@@ -85,7 +86,7 @@ export function CustomCombobox<T extends ComboboxItem>({
                         </span>
                       ) : null}
                       <span
-                        className={`block truncate px-3 ${selected ? 'font-medium' : 'font-normal'}`}
+                        className={`block truncate px-3 ${selected ? 'font-bold' : 'font-normal'}`}
                       >
                         {item.name}
                       </span>
@@ -95,6 +96,7 @@ export function CustomCombobox<T extends ComboboxItem>({
               ))
             )}
           </Combobox.Options>
+        
         </Transition>
       </div>
     </Combobox>
