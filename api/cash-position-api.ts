47b9d7db53d0945f46eb4fcd7963a1d9a760ae0b
Rifatul Islam <rifatulcse@gmail.com>
2025-02-04 16:54:38 +0600

    feat: cash position report

--- api/cash-position-api.ts
+++ api/cash-position-api.ts
@@ -1,38 +1,22 @@
 import { fetchApi } from '@/utils/http'
 import { BankBalance, CashBalance } from '@/utils/type'
 
-export async function getBankBalance() {
+export async function getBankBalance(fromDate: string, toDate: string) {
   return fetchApi<BankBalance[]>({
-    // url: `api/cash/bankBal?fromdate=${fromdate}&todate=${enddate}`,
-    url: 'api/cash/bankBal?fromdate=01-01-2024&todate=02-03-2025',
+    url: `api/cash/bankBal?fromdate=${fromDate}&todate=${toDate}`,
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
   })
 }
-export async function getCashBalance({
-  fromdate,
-  enddate,
-}: {
-  fromdate: string
-  enddate: string
-}) {
+
+export async function getCashBalance(fromDate: string, toDate: string) {
   return fetchApi<CashBalance[]>({
-    // url: `api/cash/bankBal?fromdate=${fromdate}&todate=${enddate}`,
-    url: 'api/cash/cashBal?fromdate=01-01-2024&todate=02-03-2025',
+    url: `api/cash/cashBal?fromdate=${fromDate}&todate=${toDate}`,
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
   })
 }
-
-
-// {
-//   fromdate,
-//   enddate,
-// }: {
-//   fromdate: string
-//   enddate: string
-// }
