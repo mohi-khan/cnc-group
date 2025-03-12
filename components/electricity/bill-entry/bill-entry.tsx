a3a8bcdf25ff1cba04f568166882abdb34f5117e
Rifatul Islam <rifatulcse@gmail.com>
2025-03-12 15:50:41 +0600

    feat: bill entry complete

--- components/electricity/bill-entry/bill-entry.tsx
+++ components/electricity/bill-entry/bill-entry.tsx
@@ -1,11 +1,25 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BillEntryList from './bill-entry-list'
 import BillEntryPopUp from './bill-entry-popup'
+import { GetElectricityBillType } from '@/utils/type'
+import { getBillEntry } from '@/api/bill-entry-api'
 
 const BillEntry = () => {
 
   const [isPopupOpen, setIsPopupOpen] = useState(false)
+   const [billEntry, setBillEntry] = React.useState<GetElectricityBillType[]>([])
+    
+  
+    const fetchBillEntry = async () => {
+      const response = await getBillEntry()
+      setBillEntry(response.data ?? [])
+      console.log('🚀 ~get meter entry data :', response)
+    }
+  
+    useEffect(() => {
+      fetchBillEntry()
+    }, [])
       
   
   
@@ -17,16 +31,17 @@ const BillEntry = () => {
        
         setIsPopupOpen(false)
       }
-  return <div>
-    <BillEntryList
-      onAddCategory={handleAddCategory}
-    />
-    <BillEntryPopUp
-      isOpen={isPopupOpen}
-      onOpenChange={setIsPopupOpen}
-      onCategoryAdded={handleCategoryAdded}
-    />
-  </div>
+  return (
+    <div>
+      <BillEntryList onAddCategory={handleAddCategory} billEntry={billEntry} />
+      <BillEntryPopUp
+        isOpen={isPopupOpen}
+        onOpenChange={setIsPopupOpen}
+        onCategoryAdded={handleCategoryAdded}
+          fetchBillEntry={fetchBillEntry}
+      />
+    </div>
+  )
 }
 
 export default BillEntry
