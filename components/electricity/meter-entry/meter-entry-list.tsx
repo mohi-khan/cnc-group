5f0caa5b032a8cedbe094cc8554eeaea635f2785
Rifatul Islam <rifatulcse@gmail.com>
2025-03-12 14:14:04 +0600

    feat: meter entry full complete and wip: bill entry

--- components/electricity/meter-entry/meter-entry-list.tsx
+++ components/electricity/meter-entry/meter-entry-list.tsx
@@ -1,62 +1,40 @@
-
 'use client'
+
+import { getMeterEntry } from '@/api/meter-entry-api'
 import { Button } from '@/components/ui/button'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { GetElectricityMeterType } from '@/utils/type'
 import { Plus } from 'lucide-react'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 interface MeterEntryListProps {
   onAddCategory: () => void
 }
 
 const MeterEntryList: React.FC<MeterEntryListProps> = ({ onAddCategory }) => {
-    const data = [
-      {
-        meterName: 'Meter 1',
-        companyName: 'Company A',
-        meterType: 'Type A',
-        costCenter: 'Cost Center 1',
-        meterDescription: 'Description of Meter 1',
-        provisionAccountName: 'Account 1',
-        expenseAccountName: 'Expense 1',
-      },
-      {
-        meterName: 'Meter 2',
-        companyName: 'Company B',
-        meterType: 'Type B',
-        costCenter: 'Cost Center 2',
-        meterDescription: 'Description of Meter 2',
-        provisionAccountName: 'Account 2',
-        expenseAccountName: 'Expense 2',
-      },
-      {
-        meterName: 'Meter 3',
-        companyName: 'Company C',
-        meterType: 'Type C',
-        costCenter: 'Cost Center 3',
-        meterDescription: 'Description of Meter 3',
-        provisionAccountName: 'Account 3',
-        expenseAccountName: 'Expense 3',
-      },
-      {
-        meterName: 'Meter 4',
-        companyName: 'Company D',
-        meterType: 'Type A',
-        costCenter: 'Cost Center 4',
-        meterDescription: 'Description of Meter 4',
-        provisionAccountName: 'Account 4',
-        expenseAccountName: 'Expense 4',
-      },
-      {
-        meterName: 'Meter 5',
-        companyName: 'Company E',
-        meterType: 'Type B',
-        costCenter: 'Cost Center 5',
-        meterDescription: 'Description of Meter 5',
-        provisionAccountName: 'Account 5',
-        expenseAccountName: 'Expense 5',
-      },      // Add more data here as needed
-    ]
+ 
+
+  const [meterEntry, setMeterEntry] = React.useState<GetElectricityMeterType[]>(
+    []
+  )
+
+  const fetchMeterEntry = async () => {
+    const response = await getMeterEntry()
+    setMeterEntry(response.data ?? [])
+    console.log('🚀 ~get meter entry data :', response)
+  }
+
+  useEffect(() => {
+    fetchMeterEntry()
+  }, [])
+
   return (
     <div className="p-4">
       <div className="flex justify-between items-center mb-6">
@@ -68,7 +46,7 @@ const MeterEntryList: React.FC<MeterEntryListProps> = ({ onAddCategory }) => {
           ADD
         </Button>
       </div>
-      <div >
+      <div>
         <Table className="shadow-md border ">
           <TableHeader className="bg-slate-200 shadow-md ">
             <TableRow>
@@ -82,15 +60,15 @@ const MeterEntryList: React.FC<MeterEntryListProps> = ({ onAddCategory }) => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {data.map((row, index) => (
+            {meterEntry.map((row, index) => (
               <TableRow key={index}>
                 <TableCell>{row.meterName}</TableCell>
                 <TableCell>{row.companyName}</TableCell>
-                <TableCell>{row.meterType}</TableCell>
-                <TableCell>{row.costCenter}</TableCell>
-                <TableCell>{row.meterDescription}</TableCell>
-                <TableCell>{row.provisionAccountName}</TableCell>
-                <TableCell>{row.expenseAccountName}</TableCell>
+                <TableCell>{row.metertpe}</TableCell>
+                <TableCell>{row.costCenterName}</TableCell>
+                <TableCell>{row.description}</TableCell>
+                <TableCell>{row.provaccountName}</TableCell>
+                <TableCell>{row.accountHead}</TableCell>
               </TableRow>
             ))}
           </TableBody>
