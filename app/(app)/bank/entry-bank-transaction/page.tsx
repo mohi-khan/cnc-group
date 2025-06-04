eef65bd0dc4b2db15a52263e40c325c880c93a21
Akil Tajwar <akiltajwar@gmail.com>
2025-06-04 16:55:19 +0600

    wip: bank-transaction page

---
+++ app/(app)/bank/entry-bank-transaction/page.tsx
@@ -0,0 +1,12 @@
+import EntryBankTransaction from '@/components/bank/entry-bank-transaction/entry-bank-transaction'
+import React from 'react'
+
+const EntryBankTransactionPage = () => {
+  return (
+    <div>
+      <EntryBankTransaction />
+    </div>
+  )
+}
+
+export default EntryBankTransactionPage
