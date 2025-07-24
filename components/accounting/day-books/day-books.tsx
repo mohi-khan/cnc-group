55ca9ba1ef33b6a99d5bb4b2dda691e775411623
Akil Tajwar <akiltajwar@gmail.com>
2025-07-24 16:58:43 +0600

    refactor: day books => adding edit button

--- components/accounting/day-books/day-books.tsx
+++ components/accounting/day-books/day-books.tsx
@@ -164,7 +164,6 @@ const DayBooks = () => {
     { key: 'companyname', label: 'Company Name' },
     { key: 'currency', label: 'Currency' },
     { key: 'location', label: 'Location' },
-    { key: 'date', label: 'Date' },
     { key: 'notes', label: 'Remarks' },
     { key: 'totalamount', label: 'Total Amount' },
     { key: 'state', label: 'Status' },
