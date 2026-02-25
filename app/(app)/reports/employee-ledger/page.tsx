00d7130d75a50ddd2455a1789be4555981fdaad9
Akil Tajwar <akiltajwar@gmail.com>
2026-02-25 10:46:03 +0600

    feat: employee ledger page

---
+++ app/(app)/reports/employee-ledger/page.tsx
@@ -0,0 +1,12 @@
+import EmployeeLedger from '@/components/reports/employee-ledger/employee-ledger';
+import React from 'react';
+
+const EmployeeLedgerPage = () => {
+    return (
+        <div>
+            <EmployeeLedger />
+        </div>
+    );
+};
+
+export default EmployeeLedgerPage;
